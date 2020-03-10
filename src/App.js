df7156da087623fa52f8714e892d4dd048539e13
david <davidealvarez1@gmail.com>
2020-03-10 12:54:21 -0700

    added bootstrap and reactstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {Button} from "reactstrap";
 
 function App() {
   return (
     <div className="App">
       <h1>GitHub Usercard</h1>
+      <Button color="danger">danger</Button>{' '}
     </div>
   );
 }
